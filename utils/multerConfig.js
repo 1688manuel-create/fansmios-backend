a86689f840c28f9b7223822a77b16d7a6593c69e
1688manuel-create <1688.manuel@gmail.com>
2026-03-26 11:50:00 -0700

    fix: publicador blindado al 100%

--- utils/multerConfig.js
+++ utils/multerConfig.js
@@ -15,25 +15,25 @@ const storage = multer.diskStorage({
     cb(null, uploadDir); // Las guardamos en backend/uploads/
   },
   filename: function (req, file, cb) {
-    // Le ponemos un nombre único: Fecha + Número Random + Extensión original (.jpg, .png)
+    // Le ponemos un nombre único: Fecha + Número Random + Extensión original (.jpg, .png, .mp4)
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, uniqueSuffix + path.extname(file.originalname));
   }
 });
 
-// 3. Filtro de Seguridad (Solo aceptamos imágenes)
+// 3. Filtro de Seguridad (Aceptamos imágenes y videos)
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/')) {
-    cb(null, true); // Es imagen, la dejamos pasar
+  if (file.mimetype.startsWith('image/') || file.mimetype.startsWith('video/')) {
+    cb(null, true); // Es imagen o video, lo dejamos pasar 🟢
   } else {
-    cb(new Error('¡Solo se permiten archivos de imagen!'), false); // Bloqueado
+    cb(new Error('¡Solo se permiten archivos de imagen y video!'), false); // Bloqueado 🔴
   }
 };
 
-// 4. Creamos el objeto final con un límite de peso (5MB máximo)
+// 4. Creamos el objeto final con un límite de peso (100MB máximo)
 const upload = multer({ 
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB
+  limits: { fileSize: 100 * 1024 * 1024 }, // 100 MB reales
   fileFilter: fileFilter
 });
 
