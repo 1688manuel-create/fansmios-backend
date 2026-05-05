576620c10791703c7e255a4f97607b4e8e602164
1688manuel-create <1688.manuel@gmail.com>
2026-05-04 21:25:29 -0700

    feat(live): esteroides de video a 1080p, simulcast y camara frontal

--- utils/cloudinaryConfig.js
+++ utils/cloudinaryConfig.js
@@ -15,34 +15,50 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: async (req, file) => {
-    // Valores por defecto para posts normales
     let folderName = 'fansmio_uploads';
     let transformations = [];
+    let resourceType = 'auto';
 
     // 🔥 MAGIA: Si detecta que están subiendo un Avatar
     if (file.fieldname === 'profileImage') {
       folderName = 'fansmio_avatares';
-      // Cuadrado perfecto (400x400), detecta la cara y recorta alrededor de ella
       transformations = [{ width: 400, height: 400, crop: 'fill', gravity: 'face', quality: 'auto' }];
     } 
     // 🔥 MAGIA: Si detecta que están subiendo una Portada
     else if (file.fieldname === 'coverImage') {
       folderName = 'fansmio_portadas';
-      // Rectángulo panorámico (1920x1080), centrado
       transformations = [{ width: 1920, height: 1080, crop: 'fill', gravity: 'center', quality: 'auto' }];
+    }
+    // 🛑 NUEVA REGLA TÁCTICA: Blindaje para Videos
+    else if (file.mimetype.startsWith('video/')) {
+      folderName = 'fansmio_videos';
+      resourceType = 'video';
+      transformations = [
+        { 
+          width: 1920, 
+          height: 1080, 
+          crop: 'limit', // 'limit' baja los 4K a 1080p, pero NO estira los videos de 720p (evita que se pixelen)
+          quality: 'auto', 
+          duration: "300" // Guillotina: Corta automáticamente a 5 minutos (300 segundos)
+        }
+      ];
     }
 
     return {
       folder: folderName,
       allowed_formats: ['jpg', 'png', 'jpeg', 'mp4', 'webp', 'mov', 'webm', 'mp3', 'wav', 'ogg'],
-      resource_type: 'auto',
-      // Solo aplicamos transformación si es foto de perfil o portada
+      resource_type: resourceType,
       transformation: transformations.length > 0 ? transformations : undefined
     };
   }
 });
 
-// 3. Creamos el "cargador" maestro
-const uploadCloudinary = multer({ storage: storage });
+// 3. Creamos el "cargador" maestro con LÍMITE DE PESO
+const uploadCloudinary = multer({ 
+  storage: storage,
+  limits: {
+    fileSize: 150 * 1024 * 1024 // 🔥 Límite de 150 MB por archivo. Evita ataques de denegación de servicio (DDoS) o saturación.
+  }
+});
 
 module.exports = { cloudinary, uploadCloudinary };
